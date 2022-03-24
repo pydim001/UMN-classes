97d676fc49becf78b6cc530e4c8cb2794dbdacb5
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-03-23 22:59:56 -0500

    Reviews

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="strip">
-    <img src="../assets/umn.png" id="logo" />
+    <div id="img-div">
+      <img src="../assets/umn.png" id="logo" />
+    </div>
     <div id="title">
       <h1>UMN CLASSES</h1>
+    </div>
+    <div id="btn-div">
+      <button id="review">REVIEW</button>
     </div>
   </div>
 </template>
@@ -20,16 +25,33 @@ export default {
 #logo {
   height: 50px;
   position: relative;
-  right: 650px;
+  display: inline-block;
+  right: -20px;
 }
 #title {
   color: gold;
   text-align: center;
   margin-top: -70px;
+  display: inline-block;
+  margin-top: -20px;
 }
 #strip {
   background-color: maroon;
-  height: 50;
+  height: 50px;
   text-align: center;
+}
+#review {
+  background-color: gold;
+  color: maroon;
+}
+#btn-div {
+  display: inline-block;
+  position: absolute;
+  left: 1300px;
+  margin-top: 10px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+#img-div {
+  margin-bottom: -53px;
 }
 </style>
